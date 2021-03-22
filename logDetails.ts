d3d9e3dd543c4aa7bea262acead5bbe592d45d9b
Emerson Silva <emerson_jdss@hotmail.com>
2021-03-22 15:45:50 -0300

    type alias and union

---
+++ logDetails.ts
@@ -0,0 +1,21 @@
+type Uid = number | string // type alias
+
+// when we use | it's a union type in Typescript
+var firstName: string | undefined;
+
+function logDetails(uid: Uid, item: string) {
+  console.log(`a product with ${uid} has a title ${item}`)
+}
+
+function logInfo(id: Uid, user: string) {
+  console.log(`an user with ${id} has a name ${user}`)
+}
+
+type Platform = 'Windows' | 'Linux' | 'Mac OS'
+
+function renderPlatform(platform: Platform) {
+  return platform
+}
+
+logDetails(123, 'sapato');
+logDetails('123', ' sapato');
