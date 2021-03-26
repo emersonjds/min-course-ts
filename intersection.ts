22d6009f4d87f7e4f152a86281d5a3e9dd617b34
Emerson Silva <emerson_jdss@hotmail.com>
2021-03-25 23:42:47 -0300

    working with intersection

---
+++ intersection.ts
@@ -0,0 +1,34 @@
+//PlayerInfo
+//UseInfo
+//CharInfo
+
+type UserInfo = {
+    id: number
+    name: string
+    // optional
+    email?: string
+}
+
+const userInfo: UserInfo = {
+    id: 1,
+    name: 'Emerson'
+}
+
+type CharInfo = {
+    level: number
+    nickName: string
+}
+
+const char: CharInfo = {
+    level: 100,
+    nickName: 'major'
+}
+
+type PlayerInfo = UserInfo & CharInfo
+
+const player: PlayerInfo = {
+    id: 10,
+    name: 'Emerson',
+    level: 5,
+    nickName: 'major'
+}
