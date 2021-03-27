b8cc0f2964f922e8ca617a02f73b453cd1a5af5e
Emerson Silva <emerson_jdss@hotmail.com>
2021-03-26 23:54:38 -0300

    cconfig classes

--- classes.ts
+++ classes.ts
@@ -1,3 +1,24 @@
 class Person {
+    name: string
+    age: number
+
+    constructor(name: string, age: number) {
+        this.name = name
+        this.age = age
+    }
+}
+
+const person = new Person("Emerson",31)
+
+class Professional extends Person {
+    
+    rule: string
+    address: string
+
+    constructor(rule: string, address: string) {
+        super()
+        this.rule = rule
+        this.address = address
+    }
     
 }
