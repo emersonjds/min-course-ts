fb74ca1afb56c1ca9d97322fcf86d4f1fa819824
Emerson Silva <emerson_jdss@hotmail.com>
2021-05-31 23:53:51 -0300

    changes on type-inference

--- type-inference.ts
+++ type-inference.ts
@@ -1,6 +1,6 @@
 let message2 = 'Mensagem';
 
-// document window working with type inference
+// document window working with type inference on DOM
 window.addEventListener('click', (e) => {
   console.log(e.target)
 })
